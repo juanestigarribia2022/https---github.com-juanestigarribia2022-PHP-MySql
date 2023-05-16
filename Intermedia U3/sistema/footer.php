2c34d6805278b73dac0e7fb543a10ec6632e4bd7
Juan Esteban Estigarribia <estigarribiajuanesteban@gmail.com>
2023-05-16 16:14:46 -0300

    INICIO INTERMEDIA U 3

---
+++ Intermedia U3/sistema/footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <style>
+    </style>
+    <div class="cotizaciones">
+        <div><iframe style="width:320px;height:260px;border-radius:10px;box-shadow:2px 4px 4px rgb(0 0 0 / 25%); float: left ;border:1px solid #bcbcbc" src="https://dolarhoy.com/i/cotizaciones/dolar-blue" frameborder="0"></iframe></div>
+        <div><iframe style="width:320px;height:260px;border-radius:10px;box-shadow:2px 4px 4px rgb(0 0 0 / 25%); float: left ;border:1px solid #bcbcbc" src="https://dolarhoy.com/i/cotizaciones/dolar-bancos-y-casas-de-cambio" frameborder="0"></iframe></div>
+        <div><iframe style="width:320px;height:260px;border-radius:10px;box-shadow:2px 4px 4px rgb(0 0 0 / 25%); float: left ;border:1px solid #bcbcbc" src="https://dolarhoy.com/i/cotizaciones/dolar-mep" frameborder="0"></iframe></div>
+    </div>
+    <style>
+        p {
+            font-size: 12px;
+            font-family: Verdana, Geneva, sans-serif;
+        }
+    </style>
+    <div class="diseñador">
+        <p>Diseñado por Juan Esteban Estigarribia </p>
+        <p>Podes contactarte conmigo a través de</p>
+        <a href="https://www.facebook.com/profile.php?id=100010649921559"><img src="./img/logo facebook.png" alt="img credimas/logo facebook.png"></a></h4>
+        <a href="https://www.instagram.com/juane_estigarribia/"><img src="./img/logo instagram1.png" alt="img credimas/logo instagram1.png   "></a></h4>
+        <a href="https://api.whatsapp.com/send?phone=5493517910065"><img src="./img/logo whatsapp.png" alt="img credimas/logo whatsapp.png"></a></h4>
+    </div>
+</footer>
